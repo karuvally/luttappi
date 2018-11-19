e96825f45d2e1ec5ee4dec6be3021b8468bcbd3f
Aswin Karuvally <aswinbabuk@gmail.com>
2018-11-19 07:41:43 +0530

    added skeleton for main()

---
+++ luttappi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# luttappi, alpha release
+
+
+# the main function
+def main():
+    # create files if they do not exist
+
+    # log sensors in separate thread
+
+    # start the web server
+
+
+# call the main function
+main()
