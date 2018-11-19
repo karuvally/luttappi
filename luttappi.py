a19f9691bd06c3df688b599d62c710c428054e6c
Aswin Karuvally <aswinbabuk@gmail.com>
2018-11-19 07:52:56 +0530

    created skeleton for initialize_system()

--- luttappi.py
+++ luttappi.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 # luttappi, alpha release
 
+# import the serious stuff
+import csv
+import os
+
+
+# check if essential files exist
+def initialize_system():
+    # get config_dir path
+
+    # create config_dir if not present
+    
+    # create config_file if not present
+
 
 # the main function
 def main():
     # create files if they do not exist
+    initialize_system()
 
     # log sensors in separate thread
 
