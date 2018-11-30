70c75c74108632cb4cf358509c69eb452a93ce96
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2018-11-30 14:11:37 +0530

    script exits if non-root

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Luttappi installation script
 # Copyright 2018, Aswin Babu Karuvally
+
+# get the current username
+USER_NAME=`whoami`
+
+# exit if not root
+if [ $USER_NAME != "root" ]
+then
+    echo "run script as root!"
+    exit
+fi
